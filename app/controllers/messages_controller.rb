dec317cc9df9c965749df5dc900d2382652c8ef6
shin6585 <shinyujiro@gmail.com>
2016-03-06 11:25:52 +0000

    fix controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     @messages = Message.all
@@ -13,6 +14,23 @@ class MessagesController < ApplicationController
       flash.now[:alert] = 'メッセージの保存に失敗しました'
       render 'index'
     end
+  
+  end
+  
+  def edit 
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path, notice: "メッセージを保存しました"
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: "メッセージを削除しました"
   end
   
   private
@@ -20,4 +38,7 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:name, :body, :age)
   end
   
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
